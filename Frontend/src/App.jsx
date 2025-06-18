45366c094c72c56bebda5c145079583021d6e5fa
0xZahidp <mdzahidhasanpatwary@gmail.com>
2025-06-19 02:11:48 +0600

    Just Fun

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,42 @@ function App() {
 				<img src={viteLogo} style={{ width: '100%', maxHeight: '60vh', borderRadius: '2rem' }} alt="Vite logo" />
 				<h1>Let's read books!</h1>
 			</div>
+			<div>
+<hr class="dashed"></hr>
+				<h1>This is an university Project by</h1>
+				<h1>Team Ultron </h1>
+
+			</div>
+
+			<h1>Ultron Members</h1>
+
+<table id="members">
+  <tr>
+    <th>Name</th>
+    <th>ID</th>
+  </tr>
+  <tr>
+    <td>Md Sanaullah</td>
+    <td>221-15-4995</td>
+
+  </tr>
+  <tr>
+    <td>Md Zahid Hasan Patwary</td>
+    <td>221-15-4996</td>
+  </tr>
+  <tr>
+    <td>Md Amran Haque</td>
+    <td>221-15-5662</td>
+  </tr>
+  <tr>
+    <td>Md Tahsinul Haque</td>
+    <td>221-15-4661</td>
+  </tr>
+  <tr>
+    <td>Md Shakibul Islam</td>
+    <td>221-15-5551</td>
+  </tr>
+</table>
 		</>
 	);
 }
